81df8f7df4b8fbbba71d5cda68be590d8ea0c940
Ilannkumaran Ilangovan <kumaranilan@yahoo.com>
2023-10-28 22:53:31 +0100

    Test commit

--- use-external-jar-boot2/src/main/java/com/ilan/config/OpenAPILocalConfig.java
+++ use-external-jar-boot2/src/main/java/com/ilan/config/OpenAPILocalConfig.java
@@ -1,4 +1,32 @@
 package com.ilan.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile({"default", "local"})
+@Configuration
 public class OpenAPILocalConfig {
+
+    Contact contact = new Contact()
+            .email("ilankumaran.i@gmail.com")
+            .name("Ilankumaran Ilangovan");
+
+    Info info = new Info()
+            .contact(contact)
+            .description("Spring Boot 2 + Open API 3")
+            .summary("Demo of Spring Boot 2 & Open API 3 Integration")
+            .title("Spring Boot 2 + Open API 3")
+            .version("V1.0.0")
+            .license(new License().name("Apache 2.0").url("http://springdoc.org"));
+
+    @Bean
+    public OpenAPI myOpenAPI() {
+        return new OpenAPI()
+                .info(info);
+    }
 }
---
+++ use-external-jar-boot2/src/main/java/com/ilan/config/OpenAPIProdConfig.java
@@ -0,0 +1,73 @@
+package com.ilan.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.customizers.OpenApiCustomiser;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import static org.springdoc.core.Constants.ALL_PATTERN;
+
+
+//https://www.baeldung.com/openapi-jwt-authentication
+
+@Profile("!default & !local")
+@Configuration
+public class OpenAPIProdConfig {
+
+    Server localServer = new Server()
+            .url("http://localhost:8080")
+            .description("Localhost Server URL");
+
+    Contact contact = new Contact()
+            .email("ilankumaran.i@gmail.com")
+            .name("Ilankumaran Ilangovan");
+    Info info = new Info()
+            .contact(contact)
+            .description("Spring Boot 3 + Open API 3")
+            .summary("Demo of Spring Boot 3 & Open API 3 Integration")
+            .title("Spring Boot 3 + Open API 3")
+            .version("V1.0.0")
+            .license(new License().name("Apache 2.0").url("http://springdoc.org"));
+    @Bean
+    public GroupedOpenApi apiV1() {
+        return GroupedOpenApi.builder()
+                .addOpenApiCustomiser(publicApiCustomizer())
+                .packagesToScan("com.ilan", "org.jpmc")
+                .pathsToMatch(ALL_PATTERN)
+                .group("External API")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI myOpenAPI() {
+        return new OpenAPI()
+                .info(info)
+                .addServersItem(localServer);
+    }
+
+    @Bean
+    OpenApiCustomiser publicApiCustomizer(){
+        final String securitySchemeName = "bearerAuth";
+        final String authSchemeName = "Authorization";
+        return openApi -> openApi.addSecurityItem(new SecurityRequirement()
+                        //.addList(securitySchemeName)
+                        .addList(authSchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                //.name(securitySchemeName)
+                                .in(SecurityScheme.In.HEADER)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+
+}
